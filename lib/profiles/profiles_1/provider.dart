040ec4ced3d2211a3becdcee4b1c62ddf25a7be7
abdedayem yosri <60704101+yosri9@users.noreply.github.com>
2020-10-17 20:46:40 +0200

    profile1

---
+++ lib/profiles/profiles_1/provider.dart
@@ -0,0 +1,23 @@
+
+
+
+
+import 'user.dart';
+
+class ProfileProvider{
+
+  static Profile getProfile(){
+
+    return Profile(
+      user: User(
+        name: 'Erik Walters',
+        address: '677 adams Bypass',
+        about: "Manoeuvering small talk situations is an important skill — especially in the business context. To help you improve it, we have put together some examples of"
+      ),
+      followers: 2318,
+      following: 364,
+      friends: 175
+    );
+
+  }
+}
